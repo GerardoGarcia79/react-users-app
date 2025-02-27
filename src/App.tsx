1b98f0918f9cbbebda7ac93906cfd3db25f668cb
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-27 14:07:22 -0600

    Get Users

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+import { APIResponse, Result } from "./types/api-response";
+
 function App() {
+  const [users, setUsers] = useState<Result[]>();
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
+    const url = "https://randomuser.me/api/?page=1&results=10&seed=abc";
+    getUsers(url, controller.signal);
+
+    return () => controller.abort();
+  }, []);
+
+  const getUsers = async (url: string, signal: AbortSignal) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(url, { signal });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const data: APIResponse = await response.json();
+      setUsers(data.results);
+      setIsLoading(false);
+    } catch (error) {
+      if (error instanceof Error && error.name === "AbortError") return;
+
+      if (error instanceof Error)
+        setError(`An unexpected error occurred: ${error.message}`);
+
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <h1>Hello World!</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {JSON.stringify(users)}
     </>
   );
 }
---
+++ src/types/user.d.ts
@@ -0,0 +1,9 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  address: string;
+  age: number;
+  cell: string;
+  picture: string;
+}
